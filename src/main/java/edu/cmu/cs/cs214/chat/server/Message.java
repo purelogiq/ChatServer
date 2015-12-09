763951fdc76978c884922195ae92f1d95ce6ea67
Israel M <purelogiq@gmail.com>
2015-12-09 15:06:47 -0500

    Removed todo statements

--- src/main/java/edu/cmu/cs/cs214/chat/server/Message.java
+++ src/main/java/edu/cmu/cs/cs214/chat/server/Message.java
@@ -16,10 +16,6 @@ public class Message implements Serializable {
     private String sender;
     private Date serverTimestamp;
 
-
-    // TODO: Make the message also include a timestamp for when the server
-    // received the message
-
     /**
      * Constructor to create a message
      * 
